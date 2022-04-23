759cb84f29301c96e6bbadec5e98ac55541620ee
Fernando Medeiros <fernandohenrique520@live.com>
2022-04-22 21:55:23 -0300

    refactory da Api e inclusao do JWT

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,11 +1,8 @@
-/*
+
 const bodyParser = require('body-parser');
 const cors = require('cors');
-//const Sequelize = require('sequelize');
 
 module.exports = app => {
     app.use(bodyParser.json())
     app.use(cors())
-    //app.use(Sequelize)
 }
-*/
