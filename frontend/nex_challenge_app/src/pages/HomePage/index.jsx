2bc96065110c1ff8770bfc845d526d31c4b03e12
Fernando Medeiros <fernandohenrique520@live.com>
2022-04-25 00:26:39 -0300

    Validações na Pagina de Login e ajuste na HomePage

--- frontend/nex_challenge_app/src/pages/HomePage/index.jsx
+++ frontend/nex_challenge_app/src/pages/HomePage/index.jsx
@@ -6,6 +6,10 @@ import { getProducts } from "../../services/api";
 
 import Main from '../../components/template/Main'
 
+import jpIMG from "../../assets/img/logo3.png";
+
+import "./style.css"
+
 
 
 const HomePage = () => {
@@ -25,26 +29,34 @@ const HomePage = () => {
         logout();
     };
 
+    const userLogged = localStorage.getItem("user_name");
+    const userActive = JSON.parse(userLogged)
+
     if(loading){
         return <div className="loading">Carregando...</div>
     }
 
     return(
-    <Main icon="home" title="Início" subtitle="MY STORE" >
-        <div className='display-4'>Bem vindo!</div>
+    <Main icon="ship" title="Início" subtitle="MY STORE" >
+        <div className='display-4'> <img src={jpIMG} alt="logo"></img> BUCCANEERS TEC STORE</div>
         <hr/>
-        <p className='mb-0'>Uma Lista Completa de produtos a sua inteira diposição.</p>
+        <h3>Bem vindo, {userActive}</h3>
+        <button className="btn btn-danger" onClick={handleLogout}>Sair</button>
         <hr/>
-        <button className="btn btn-danger" onClick={handleLogout}>Logout</button>
-        <hr/>
-        <ul>
-            {products.map((product) => (
-                <li key={product.products_id}>
-                    {product.product_name} - {product.product_price}
-                </li>
+        <div className="row">
+        {products.map((product) =>(
+            <div key={product.products_id} className="col-12 col-md-3">
+                <div   className="card">
+                    <div className="card-body">
+                        <h4 className=" card-title">{product.product_name}</h4>
+                        <h5 className=" card-title">$BRL:{product.product_price},00</h5>
+                        <p className="card-text">{product.product_description}</p>
+                        <img src={product.product_img} alt="product_image"></img>
+                    </div>
+                </div>
+            </div>
             ))}
-        </ul>
-        
+        </div>
     </Main>
     )
 };
